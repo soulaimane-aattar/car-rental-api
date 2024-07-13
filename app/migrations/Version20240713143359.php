75cba09583e9c99d14a302b8fc565569afdaed20
soulaimane aattar <soulaimane.aattar@gmail.com>
2024-07-13 15:38:01 +0100

    feat: Add migration for creating car and reservation tables

---
+++ app/migrations/Version20240713143359.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240713143359 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE car ADD year VARCHAR(255) NOT NULL, DROP available');
+        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C84955A76ED395');
+        $this->addSql('DROP INDEX IDX_42C84955A76ED395 ON reservation');
+        $this->addSql('ALTER TABLE reservation DROP user_id');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE car ADD available TINYINT(1) NOT NULL, DROP year');
+        $this->addSql('ALTER TABLE reservation ADD user_id INT NOT NULL');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C84955A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('CREATE INDEX IDX_42C84955A76ED395 ON reservation (user_id)');
+    }
+}
